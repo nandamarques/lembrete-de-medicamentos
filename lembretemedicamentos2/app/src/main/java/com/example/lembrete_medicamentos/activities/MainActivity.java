533513299bbcc5bef17b1064ece3c380df682ba0
brprado <brunopradocode@gmail.com>
2023-04-18 19:03:37 -0300

    Sugestao de alteracao do metodo de notificacao. Nao consegui testar por conta de algum problema no AndroidStudio. Adicionei tambem uma solicitacao para permissao de acesso a vibracao do telefone no AndroidManifest.xml
    OBS: O codigo esta comentado, exceto a solicitacao de permissao no manifest

--- lembretemedicamentos2/app/src/main/java/com/example/lembrete_medicamentos/activities/MainActivity.java
+++ lembretemedicamentos2/app/src/main/java/com/example/lembrete_medicamentos/activities/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.lembrete_medicamentos.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.R;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
-
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -13,7 +13,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.lembrete_medicamentos.R;
 
 public class MainActivity extends AppCompatActivity {
     private Button salvar;
@@ -64,4 +63,40 @@ public class MainActivity extends AppCompatActivity {
     public void Notificar(View v){
         Toast.makeText(this, "Login realizado!", Toast.LENGTH_SHORT).show();
     }
+
+//    ADICIONEI UMA PERMISSAO PARA QUE O USUARIO PERMITA O ACESSO A VIRACAO DO CELULAR
+//    private static final String CHANNEL_ID = "my_channel";
+//
+//    private void createNotificationChannel() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            CharSequence name = getString(R.string.channel_name);
+//            String description = getString(R.string.channel_description);
+//            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+//            channel.setDescription(description);
+//            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+//            notificationManager.createNotificationChannel(channel);
+//        }
+//    }
+//
+//    private void notification() {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+//                .setSmallIcon(R.drawable.ic_launcher_background)
+//                .setContentTitle("Lembrete de Remédio")
+//                .setContentText("Está no horário de tomar seu remédio")
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//                .setContentIntent(pendingIntent)
+//                .setAutoCancel(true);
+//
+//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+//        notificationManager.notify(123, builder.build());
+//    }
+//
+//    public void Notificar(View view) {
+//        Toast.makeText(this, "Login realizado!", Toast.LENGTH_SHORT).show();
+//    }
+
 }
+
